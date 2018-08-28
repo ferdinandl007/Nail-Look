a43fb26318f4be5e58dd2194baeba1685ad973bf
ferdinand <ferdinand@ferdinands-macbook-pro.local>
2018-08-28 11:38:41 +0800

    Initial Commit

---
+++ Nail Look/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Nail Look
+//
+//  Created by ferdinand on 8/28/18.
+//  Copyright © 2018 ferdinand. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
